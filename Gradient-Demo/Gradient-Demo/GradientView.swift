46c3d73027a530c4a3b6fdbba8253a2eed76ea47
ankitjaiswal1994 <ankit.jaiswal@crownstack.com>
2019-01-12 23:54:19 +0530

    Create extension for GradientView

---
+++ Gradient-Demo/Gradient-Demo/GradientView.swift
@@ -0,0 +1,35 @@
+//
+//  GradientView.swift
+//  Gradient-Demo
+//
+//  Created by Ankit Jaiswal on 12/01/19.
+//  Copyright © 2019 Ankit Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+    @IBInspectable var firstColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    @IBInspectable var secondColor: UIColor = UIColor.clear {
+        didSet {
+            updateView()
+        }
+    }
+    
+    override class var layerClass: AnyClass {
+        get {
+            return CAGradientLayer.self
+        }
+    }
+    
+    func updateView() {
+        let layer = self.layer as! CAGradientLayer
+        layer.colors = [firstColor.cgColor, secondColor.cgColor]
+    }
+}
--- Gradient-Demo/Gradient-Demo/ViewController.swift
+++ Gradient-Demo/Gradient-Demo/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let gradientLayer = CAGradientLayer()
+        
+        gradientLayer.colors = [UIColor.black.cgColor, UIColor.white.cgColor]
+        gradientLayer.frame = view.frame
+        
+        //this will be on the top of every view
+       // view.layer.addSublayer(gradientLayer)
+        
+        //this will add at the bottom at index 0
+       // view.layer.insertSublayer(gradientLayer, at: 0)
     }
 
 
