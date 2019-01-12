92f782f0efc6373392a6640f6450d1c588625cd1
ankitjaiswal1994 <ankit.jaiswal@crownstack.com>
2019-01-12 23:31:30 +0530

    Added demo project

---
+++ Gradient-Demo/Gradient-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gradient-Demo
+//
+//  Created by Ankit Jaiswal on 12/01/19.
+//  Copyright © 2019 Ankit Jaiswal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
